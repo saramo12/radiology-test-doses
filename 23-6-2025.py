39b2a95a7805c1476244d5918cfdbba48a4a398b
saramo123 <sarausama1001@gmail.com>
2025-06-23 15:37:57 +0300

    done

--- 23-6-2025.py
+++ 23-6-2025.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageTk
 import pydicom
 import os
 import numpy as np
-
 from datetime import datetime
 from datetime import datetime, timedelta
 from pydicom.errors import InvalidDicomError
@@ -13,7 +12,6 @@ from rapidfuzz import fuzz  # أسرع من fuzzywuzzy ويمكنك استبدا
 import socket
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("dark-blue")
-
 CSV_FILE = "rad.csv"
 HL7_DIR = "hl7_messages"
 os.makedirs(HL7_DIR, exist_ok=True)
